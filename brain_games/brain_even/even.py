659c3a8e24febc8832247d473ab4e20ea47a7225
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-05-13 13:20:01 +0300

    modify even.py, fix brain_even.py

--- brain_games/brain_even/even.py
+++ brain_games/brain_even/even.py
@@ -1,25 +1,27 @@
-import random
+import random, prompt
 
 
 def is_even(number: int) -> bool:
-    """Checks if given number is even."""
+    """Check if given number is even."""
     return number % 2 == 0
+
 
 def ask_question():
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    x = random.randint(0, 100)
-    if x % 2 == 0:
+    number = random.randint(1, 100)
+    print('Question: ' + str(number))
+    answer = prompt.string('Your answer: ')
+    if is_even(number):
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
-    print('Question: ' + str(x))
-    answer = input('Your answer: ')
     if answer == correct_answer:
         print('Correct!')
         return True
     else:
-        print("""'yes' is wrong answer ;(. Correct answer was 'no'.
-        Let's try again, Bill!""")
+        print(f"""'yes' is wrong answer ;(. Correct answer was 'no'.
+Let's try again, Bill!""")  # CHANGE BILL TO NAME
         return False
 
 
+print(ask_question())
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -6,7 +6,5 @@ def main():
     print(f'Welcome to the Brain Games!')
 
 
-
-
 if __name__ == '__main__':
     main()
