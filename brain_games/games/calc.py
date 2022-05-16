ebaa4b063a37023b36308a8708811c69929a5149
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-05-16 19:54:41 +0300

    add content to calc.py

---
+++ brain_games/games/calc.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3.10
+import random
+from operator import add, mul, sub
+
+DESCRIPTION = 'What is the result of the expression?'
+
+operators = ((add, '+'), (mul, '*'), (sub, "-"))
+
+
+def solution():
+    number1 = random.randint(1, 100)
+    number2 = random.randint(1, 100)
+    operator, operation_sign = random.choice(operators)
+    question = f"{number1} {operation_sign} {number2}"
+    correct_answer = str(operator(number1, number2))
+    return question, correct_answer
