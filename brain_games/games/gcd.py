b564541973042936274f624c17597d5e52f9831c
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-05-16 22:23:52 +0300

    add gcd.py

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.10
+import random
+
+DESCRIPTION = 'Find the greatest common divisor of given numbers.'
+
+
+def solution():
+    number1 = random.randint(1, 100)
+    number2 = random.randint(1, 100)
+    question = f"{number1} {number2}"
+    divider = 2
+    if number1
+
+
