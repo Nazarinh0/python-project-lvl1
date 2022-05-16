949daf03d61104e20ff3600ec7f041920c238ca9
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-05-16 12:06:18 +0300

    changes in play.py and brain_even.py

--- brain_games/brain_even/play.py
+++ brain_games/brain_even/play.py
@@ -1,16 +1,39 @@
-import prompt, even
+import prompt
+import random
 
 
-def welcome_user():
-    """Greet user after start of the game."""
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}')
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    return name
+def is_even(number: int) -> bool:
+    """Check if given number is even."""
+    return number % 2 == 0
 
 
 def play_even():
-    name = welcome_user()
-    welcome_user()
-    even.ask_question()
+    """Main logic of the Brain Even Game."""
+    print(f'Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    score = 0
+    goal = 3
+    print(f'Hello, {name}')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    while True:
+        number = random.randint(1, 100)
+        print('Question: ' + str(number))
+        answer = prompt.string('Your answer: ')
+        if is_even(number):
+            correct_answer = 'yes'
+        else:
+            correct_answer = 'no'
+        if answer == correct_answer:
+            print('Correct!')
+            score += 1
+        else:
+            print(f"""'{answer}' is wrong answer ;(. Correct answer was {correct_answer}.""")
+            print(f"""Let's try again, {name}!""")
+            break
+        if score == goal:
+            print(f'Congratulations, {name}!')
+            break
+
+
+print(play_even())
 
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3.10
-from brain_games.brain_even import even
+from brain_games.brain_even.play import play_even
 
 
 def main():
     print(f'Welcome to the Brain Games!')
+    play_even()
 
 
 if __name__ == '__main__':
