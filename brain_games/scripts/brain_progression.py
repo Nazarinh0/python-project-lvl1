e98dc8787b781f427258bc8f4aecad445297d6e0
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-05-17 16:40:40 +0300

    fixes in brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3.10
-from brain_games.games import gcd
+from brain_games.games import progression
 from brain_games.games.engine import play
 
 
 def main():
     print(f'Welcome to the Brain Games!')
-    play(gcd)
+    play(progression)
 
 
 if __name__ == '__main__':
