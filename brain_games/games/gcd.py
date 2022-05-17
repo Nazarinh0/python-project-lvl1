6bf128f9d30a87a161bd7f6022f8f64f13b73c18
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-05-17 15:14:54 +0300

    fix in gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -9,5 +9,5 @@ def solution():
     number1 = random.randint(1, 100)
     number2 = random.randint(1, 100)
     question = f'{number1} {number2}'
-    correct_answer = gcd(number1, number2)
-    return correct_answer, question
+    correct_answer = str(gcd(number1, number2))
+    return question, correct_answer
