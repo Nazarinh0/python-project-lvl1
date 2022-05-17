0c3faff3f93f006e68577f9198dda1e961a0d4ef
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-05-17 16:34:59 +0300

    add progression.py and brain_progression.py

---
+++ brain_games/games/progression.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3.10
+import random
+
+DESCRIPTION = 'What number is missing in the progression?'
+
+
+def get_progression():
+    start = random.randint(1, 20)
+    stop = random.randint(50, 100)
+    step = random.randint(3, 7)
+    return list(range(start, stop, step))
+
+
+def solution():
+    progression = get_progression()
+    correct_answer = str(random.choice(progression))
+    progression = str(progression)
+    question = progression.replace(correct_answer, "..")
+    return question, correct_answer
