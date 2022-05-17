5faf91994f59550cd76860a772725f3598a1f4da
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-05-17 17:47:21 +0300

    add brain_prime.py

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.10
+from brain_games.games import prime
+from brain_games.games.engine import play
+
+
+def main():
+    print(f'Welcome to the Brain Games!')
+    play(prime)
+
+
+if __name__ == '__main__':
+    main()
