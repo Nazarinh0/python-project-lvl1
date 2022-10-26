a289874243238873b26fb4afaddc52dfcd487850
Nazarinh0 <105105599+Nazarinh0@users.noreply.github.com>
2022-10-26 09:50:15 +0300

    Update even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -11,8 +11,10 @@ def is_even(number: int) -> bool:
 
 def solution():
     question = random.randint(1, 100)
+    
     if is_even(question):
         correct_answer = 'yes'
         return question, correct_answer
+    
     correct_answer = 'no'
     return question, correct_answer
